1a3c5210e87803243a8c5c1755d98ae99f1ae2c5
mahithathanneru61-lang <mahithathanneru61@gmail.com>
2025-12-24 10:13:45 +0530

    Create Count Characters, Words, and Lines in a File.c

---
+++ Count Characters, Words, and Lines in a File.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+int main()
+{
+    FILE *fp;
+    char ch;
+    int characters = 0, words = 0, lines = 0;
+    int inWord = 0;
+    fp = fopen("file.txt", "r");
+    if (fp == NULL)
+    {
+        printf("File cannot be opened\n");
+        return 0;
+    }
+    while ((ch = fgetc(fp)) != EOF)
+    {
+        characters++;
+
+        if (ch == '\n')
+            lines++;
+
+        if (ch == ' ' || ch == '\n' || ch == '\t')
+        {
+            inWord = 0;
+        }
+        else if (inWord == 0)
+        {
+            inWord = 1;
+            words++;
+        }
+    }
+    printf("Characters = %d\n", characters);
+    printf("Words      = %d\n", words);
+    printf("Lines      = %d\n", lines);
+    fclose(fp);
+    return 0;
+}
